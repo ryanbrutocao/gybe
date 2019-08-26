12384e9c4401168553c0daa6c6491aa4a985ae69
Ryan Brutocao <ryanbrutocao@outlook.com>
2019-08-26 16:57:56 -0600

    Updating with a working map/links to external site

--- app.js
+++ app.js
@@ -1,103 +1,63 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYnJ1dG9jYW8iLCJhIjoiY2p5dnE2c2RhMHI0czNqbXJtMHN1YTQ3eiJ9.COtpwPhOluVHKVN1oZdSwg';
- 
+
 var map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mapbox/streets-v11',
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v10',
 center: [-104.9827856, 39.7737709],
-zoom: 11.15
+zoom: 3.15
 });
- 
-map.on('load', function () {
-// Add a layer showing the places.
-map.addLayer({
-"id": "places",
-"type": "symbol",
-"source": {
-"type": "geojson",
-"data": {
-"type": "FeatureCollection",
-"features": [
-  {
-"type": "Feature",
-"properties": {
-"description": "<strong>Make it Mount Pleasant</strong><p><a href=\"http://www.mtpleasantdc.com/makeitmtpleasant\" target=\"_blank\" title=\"Opens in a new window\">Make it Mount Pleasant</a> is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>",
-"icon": "theatre"
-},
-"geometry": {
-"type": "Point",
-"coordinates": [-104.4827856, 39.4737709]
-}
-}, 
 
-// {
-// "type": "Feature",
-// "properties": {
-// "description": "<strong>Mad Men Season Five Finale Watch Party</strong><p>Head to Lounge 201 (201 Massachusetts Avenue NE) Sunday for a <a href=\"http://madmens5finale.eventbrite.com/\" target=\"_blank\" title=\"Opens in a new window\">Mad Men Season Five Finale Watch Party</a>, complete with 60s costume contest, Mad Men trivia, and retro food and drink. 8:00-11:00 p.m. $10 general admission, $20 admission and two hour open bar.</p>",
-// "icon": "theatre"
-// },
-// "geometry": {
-// "type": "Point",
-// "coordinates": [-77.003168, 38.894651]
-// }
-// }, 
+var sitesArr = [
+  ["Detroit Reservoire", 44.707006, -122.185775, "http://oceancolor.coas.oregonstate.edu/det/lan/rgb.html"],
+  ["Dexter Reservoir", 43.913507, -122.785281, "http://oceancolor.coas.oregonstate.edu/dex/lan/rgb.html"],
+  ["Otago", -46.002945, 170.626742,""],
+  ["Geren Island", 44.794042, -122.764460, "http://oceancolor.coas.oregonstate.edu/ger/lan/rgb.html"],
+  ["San Louis Reservour", 37.052383, -121.124112, "http://oceancolor.coas.oregonstate.edu/lui/lan/rgb.html"],
+  ["Oroville Reservoir", 39.556976, -121.456147, ""],
+  ["Laguna del Sauce", -34.825800, -55.066876, "http://oceancolor.coas.oregonstate.edu/sau/lan/rgb.html"],
+  ["San Francisco Bay & Delta", 38.102135, -122.050026, "http://oceancolor.coas.oregonstate.edu/sfb/lan/rgb.html"],
+  ["Stennis Space Center", 30.352250, -89.645983, "http://oceancolor.coas.oregonstate.edu/ssc/lan/rgb.html"],
+  ["Valle de Bravo", 19.192422, -100.155311, "http://oceancolor.coas.oregonstate.edu/vdb/lan/rgb.html"],
+  ["Silverwood Lake", 34.291468, -117.325807, ""],
+  ["Diamond Lake", 33.682725, -117.013607, "http://oceancolor.coas.oregonstate.edu/dia/lan/rgb.html" ],
+  ["Pyramid Lake", 40.050989, -119.545272, "http://oceancolor.coas.oregonstate.edu/pyr/lan/rgb.html" ],
+  ["Hopatcong Lake", 40.949438, -74.633434, ""],
+  ["Puget Sount", 47.726929, -122.504591,""],
+  ["Ross Island", 45.489956, -122.659128,""],
+  ["Salt Lake", 41.013424, -112.400531 ,""],
+  ["Adriatic Sea", 43.243398, 17.010662 ,""]
 
-// {
-// "type": "Feature",
-// "properties": {
-// "description": "<strong>Big Backyard Beach Bash and Wine Fest</strong><p>EatBar (2761 Washington Boulevard Arlington VA) is throwing a <a href=\"http://tallulaeatbar.ticketleap.com/2012beachblanket/\" target=\"_blank\" title=\"Opens in a new window\">Big Backyard Beach Bash and Wine Fest</a> on Saturday, serving up conch fritters, fish tacos and crab sliders, and Red Apron hot dogs. 12:00-3:00 p.m. $25.grill hot dogs.</p>",
-// "icon": "bar"
-// },
-// "geometry": {
-// "type": "Point",
-// "coordinates": [-77.090372, 38.881189]
-// }
-// }, 
+]
+for (let i=0; i<sitesArr.length; i++){
+  console.log(sitesArr[i]);
+  var geojson = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [sitesArr[i][2],sitesArr[i][1]]
+        },
+        properties: {
+          title: sitesArr[i][0],
+          link: sitesArr[i][3]
+        }
+      },
+    ]
+  };
+  // add markers to map
+  geojson.features.forEach(function(marker) {
 
- {
-"type": "Feature",
-"properties": {
-"description": "<strong>Truckeroo</strong><p><a href=\"http://www.truckeroodc.com/www/\" target=\"_blank\">Truckeroo</a> brings dozens of food trucks, live music, and games to half and M Street SE (across from Navy Yard Metro Station) today from 11:00 a.m. to 11:00 p.m.</p>",
-"icon": "music"
-},
-"geometry": {
-"type": "Point",
-"coordinates": [-104.1827856, 39.1837709]
+    // create a HTML element for each feature
+    var el = document.createElement('div');
+    el.className = 'marker';
+    
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el)
+    .setLngLat(marker.geometry.coordinates)
+    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+    .setHTML('<h3>' + marker.properties.title + '</h3><a href="' + marker.properties.link + '">' +"More Information Here" +'</a></p>'))
+    .addTo(map);
+  });
 }
-}]
-}
-},
-"layout": {
-"icon-image": "{icon}-15",
-"icon-allow-overlap": true
-}
-});
- 
-// When a click event occurs on a feature in the places layer, open a popup at the
-// location of the feature, with description HTML from its properties.
-map.on('click', 'places', function (e) {
-var coordinates = e.features[0].geometry.coordinates.slice();
-var description = e.features[0].properties.description;
- 
-// Ensure that if the map is zoomed out such that multiple
-// copies of the feature are visible, the popup appears
-// over the copy being pointed to.
-while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-}
- 
-new mapboxgl.Popup()
-.setLngLat(coordinates)
-.setHTML(description)
-.addTo(map);
-});
- 
-// Change the cursor to a pointer when the mouse is over the places layer.
-map.on('mouseenter', 'places', function () {
-map.getCanvas().style.cursor = 'pointer';
-});
- 
-// Change it back to a pointer when it leaves.
-map.on('mouseleave', 'places', function () {
-map.getCanvas().style.cursor = '';
-});
-});
---
+++ sites.js
@@ -0,0 +1,20 @@
+var sitesArr = [
+  ["Detroit Reservoire", 44.707006, -122.185775],
+  ["Dexter Reservoir", 43.913507, -122.785281],
+  ["Otago", -46.002945, 170.626742],
+  ["Geren Island", 44.794042, -122.764460],
+  ["San Louis Reservour", 37.052383, -121.124112],
+  ["Oroville Reservoir", 39.556976, -121.456147],
+  ["Laguna del Sauce", -34.825800, -55.066876],
+  ["San Francisco Bay & Delta", 38.102135, -122.050026],
+  ["Stennis Space Center", 30.352250, -89.645983],
+  ["Valle de Bravo", 19.192422, -100.155311]
+
+  
+
+
+
+]
+for (let i=0; i<sitesArr.length; i++){
+  console.log(sitesArr[i]);
+}
